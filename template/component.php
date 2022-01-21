6e2fffc0d20aeb7ea66e41b606200a1a13b69d2e
Tuna <tunanyugen@gmail.com>
2022-01-21 13:52:43 +0700

    .

--- template/component.php
+++ template/component.php
@@ -10,19 +10,7 @@ class process.env.UUID extends ExtendedComponent
     protected string $group_id = "process.env.GROUP_ID";
     protected string $name = "process.env.COMPONENT_NAME";
     protected string $description = "process.env.COMPONENT_DESCRIPTION";
-    // variables used in pug template
     protected array $variables = [];
-    // define which type of input each variable uses
-    // AVAILABLE TYPES
-    // ckeditor: for writing articles, large content
-    // checkbox: for toggling on/off
-    // input: for writing name, title, small content
-    // media-input: for inserting images/iframes
-    // multi-media-input: for inserting multiple images/iframes
-    // select: for selecting content from a dropdown list
-    // sortable: for a draggable sorting input
-    // tag-input: for selecting multiple tags
-    // textarea: for writing notes
     protected array $input_types = [];
     /**
      * Create a new component instance.
--- template/controller.php
+++ template/controller.php
@@ -5,7 +5,19 @@ use Jenssegers\Blade\Blade;
 
 class process.env.COMPONENT_NAME
 {
+	// variables used in blade template
     protected array $variables = [];
+	// define which type of input each variable uses
+    // AVAILABLE TYPES
+    // ckeditor: for writing articles, large content
+    // checkbox: for toggling on/off
+    // input: for writing name, title, small content
+    // media-input: for inserting images/iframes
+    // multi-media-input: for inserting multiple images/iframes
+    // select: for selecting content from a dropdown list
+    // sortable: for a draggable sorting input
+    // tag-input: for selecting multiple tags
+    // textarea: for writing notes
     protected array $input_types = [];
     
 	public function index()
